3a9cb24726a3529bdf65c1541560380e82321215
Mason Cole <mcole18@asu.edu>
2019-03-21 23:46:42 -0700

    fix typos and tags

--- _DebugDemo/src/GetDressed.java
+++ _DebugDemo/src/GetDressed.java
@@ -1,6 +1,8 @@
 import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.BorderLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,28 +10,25 @@ import java.io.IOException;
 /**
  * Visual representation of how stack frames relate to method calls
  * using clothing analogy in <link>vid2</link>
+ *
+ * Note: This is not a good example of using swing, in fact
+ * for that, it is a bad example. =] It is only meant to supply
+ * a visual alongside the debugger.
  */
 public class GetDressed extends JFrame {
 
+    static final int _TICK = 1000;
     JFrame frame;
-    JPanel pane1;
-    JPanel pane2 = new JPanel();
-
+    JLabel stackLabel;
+    JLabel picLabel;
     BufferedImage stack;
     BufferedImage body;
 
-    JLabel stackLabel;
-    JLabel picLabel;
-
     String[] stackImages = {"resources/baseMainFrameStack.png", "resources/sweaterStack.png", "resources/puffyStack.png"};
-
+    static String[] bodyImages = {"resources/baseLayer.png","resources/middleLayer.png","resources/topLayer.png"};
 
     public GetDressed(int i) {
         frame = new JFrame("Getting Dressed!");
-        pane1 = new JPanel();
-        pane2 = new JPanel();
-        //frame.add(pane1, BorderLayout.WEST);
-        //frame.add(pane2, BorderLayout.EAST);
         changeState(stackImages, bodyImages, i);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -63,11 +62,9 @@ public class GetDressed extends JFrame {
     public void removeLayer() throws InterruptedException{
         setVisible(false);
         frame.dispose();
-        Thread.sleep(1000);
+        Thread.sleep(_TICK);
 
     }
-
-    static String[] bodyImages = {"resources/baseLayer.png","resources/middleLayer.png","resources/topLayer.png"};
 
     /**
      * Demo starts here.
@@ -81,14 +78,16 @@ public class GetDressed extends JFrame {
         GetDressed a = new GetDressed(0); //create base layer in main frame
         String myStacksClothes = setMessage(0);
         String myThings = "";
-        Thread.sleep(2000);
+        Thread.sleep(_TICK*2);
         myStacksClothes = putOnSweater(1, myThings); //step in
+        Thread.sleep(_TICK);
+        System.exit(0);
     }
 
     public static String putOnSweater(int i, String myThings) throws InterruptedException{
         GetDressed newLayer = new GetDressed(i); //create visual of putOnSweater change
         String myStacksClothes = setMessage(i);
-        Thread.sleep(2000);
+        Thread.sleep(_TICK);
 
         myThings = myThings + "keys wallet";
 
@@ -102,7 +101,7 @@ public class GetDressed extends JFrame {
     public static String putOnPuffyJacket(int i, String myThings) throws InterruptedException{
         GetDressed newLayer = new GetDressed(i); //create visual of putOnPuffyJacket frame
         String myStacksClothes = setMessage(i);
-        Thread.sleep(2000);
+        Thread.sleep(_TICK);
 
         myThings = myThings + " phone";
 
@@ -130,7 +129,7 @@ public class GetDressed extends JFrame {
     public static void getNewLayer(int i) throws InterruptedException{
         GetDressed newLayer = new GetDressed(i);
         String myStacksClothes = setMessage(i);
-        Thread.sleep(2000);
+        Thread.sleep(_TICK);
         if(++i < 3){
             getNewLayer(i);
         }
